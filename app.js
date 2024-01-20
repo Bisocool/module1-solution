7a0f617fe7eb4b09778fc1036414820b39bfbbad
Anish <reachanish3657@gmail.com>
2024-01-20 09:53:39 -0600

    Modified

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ LunchController.$inject = ['$scope'];
 function LunchController($scope) {
     $scope.message = "Checking...";
     $scope.items = "";
-        
     $scope.lunchCheck = function () {
         var newItems = $scope.items.split(",");
         for (let i = 0; i < newItems.length + 1; i++) {
             i += 1;
             if (i <= 3) {
-                    $scope.message = "Enjoy"
+                $scope.message = "Enjoy"
+            } else if (i > 3) {
+                $scope.message = "Too much!"
+                
+            } else {
+                $scope.message = "Please enter data first."
             }
-            if (i > 3) {
-                    $scope.message = "Too much!"
-            } 
                 
         }
             
